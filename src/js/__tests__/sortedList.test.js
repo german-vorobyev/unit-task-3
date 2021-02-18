04f9741ce4dc9606750c88d754974073ef182f24
german-vorobyev <german.v93@mail.ru>
2021-02-18 18:39:40 +0300

    four commit

--- src/js/__tests__/sortedList.test.js
+++ src/js/__tests__/sortedList.test.js
@@ -1,6 +1,20 @@
 import sortedArray from '../sortedList';
 
-test('sorted list', () => {
+test('sorted list toBe', () => {
+  const result = sortedArray([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+
+  expect(result).not.toBe([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
+
+test('sorted list toEqual', () => {
   const result = sortedArray([
     { name: 'мечник', health: 10 },
     { name: 'маг', health: 100 },
